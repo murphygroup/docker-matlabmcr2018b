257f3345029eb720fff0709c153ac8ffdd499494
Ivan E. Cao-Berg <icaoberg@andrew.cmu.edu>
2019-01-23 15:10:07 -0500

    Added build file

---
+++ build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+wget --quiet -nc http://ssd.mathworks.com/supportfiles/downloads/R2018b/deployment_files/R2018b/installers/glnxa64/MCR_R2018b_glnxa64_installer.zip
+
+if [ -d mcr-install ]; then
+	rm -rfv mcr-install
+fi
+
+mkdir mcr-install
+unzip MCR_R2018b_glnxa64_installer.zip -d mcr-install
+
+docker build -t icaoberg/matlabmcr2018b .
